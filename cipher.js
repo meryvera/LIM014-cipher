3d94313bbffb72a6f95bd156631c23a9fde811cf
meryvera <yrem.vera.5@gmail.com>
2021-01-21 09:00:58 -0500

    Updates

---
+++ cipher.js
@@ -0,0 +1,39 @@
+
+const cipher = {
+
+  encode: function (offset, encodetext){
+    let encodetextarrayjs = encodetext.split("");
+    for (let i = 0; i < encodetextarrayjs.length; i++) {
+      //if(encodetextarrayjs[i] === encodetextarrayjs[i].toUpperCase()){
+        encodetextarrayjs[i] = encodetextarrayjs[i].charCodeAt(0);
+      // } else if (encodetextarrayjs[i] === encodetextarrayjs[i].toLowerCase()) {
+      //     encodetextarrayjs[i] = encodetextarrayjs[i].toUpperCase().charCodeAt(0);
+      // }
+    }
+    console.log(encodetextarrayjs);
+
+    let runencodetextarrayjs = encodetextarrayjs.map(function(element) {
+      console.log(offset)
+      return String.fromCharCode((element - 65 + offset) % 26 + 65);
+    })
+    let stringconvert = runencodetextarrayjs.join('');
+    return stringconvert//.toLowerCase();
+  },
+
+  decode: function (number1, text1){
+    let decodetextarrjs = text1.split('');
+    console.log(decodetextarrjs);
+
+    for (let index = 0; index <decodetextarrjs.length; index++) {
+      decodetextarrjs[index] = decodetextarrjs[index].charCodeAt(0);
+    }
+    let rundecodetextarrjs = decodetextarrjs.map(function (elemento){
+      return String.fromCharCode((elemento + 65 - number1)% 26 + 65)
+    });
+    let stringconvert1 = rundecodetextarrjs.join('');
+    console.log(stringconvert1);
+    return stringconvert1;
+  }
+};
+
+export default cipher;
